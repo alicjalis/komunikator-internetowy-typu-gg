af4169fc4a9feb472f6f097c08f42772708c3d0a
alicjalis <57289626+alicjalis@users.noreply.github.com>
2024-01-29 12:32:59 +0100

    added threads wip

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import time
+import select
 
 def connect_to_server(hostname, port):
    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -25,6 +26,9 @@ def receive_id(client_socket):
 def send_message(client_socket, message):
    client_socket.sendall(message.encode('utf-8'))
 
+
+
+
 def receive_message(client_socket):
    msg_buffer = bytearray()
    while True:
@@ -34,6 +38,26 @@ def receive_message(client_socket):
        msg_buffer += chunk
    return msg_buffer.decode('utf-8')
 
+def send_messages(client_socket):
+    while True:
+        receiver_id = input("Enter the id of the user you want to send a message to: ")
+        send_id(client_socket, receiver_id)
+        time.sleep(0.1) # Opóźnienie 0.1 sekundy
+        message = input("Enter your message: ")
+        send_message(client_socket, message)
+
+
+def check_messages(client_socket):
+    while True:
+        # Sprawdź, czy na gnieździe klienta są jakieś wiadomości do odbioru
+        ready_to_read, _, _ = select.select([client_socket], [], [], 0)
+        if ready_to_read:
+            # Jeżeli na gnieździe klienta są jakieś wiadomości do odbioru, odczytaj je
+            msg = client_socket.recv(1024).decode()
+            print('Received message: ', msg)
+        else:
+            # Jeżeli na gnieździe klienta nie ma żadnych wiadomości do odbioru, przejdź do następnej iteracji pętli
+            continue
 
 def main():
    hostname = "192.168.80.130"
@@ -43,31 +67,15 @@ def main():
    client_socket = connect_to_server(hostname, port)
    send_username(client_socket, username)
    print("Your id is: ", receive_id(client_socket))
+   #to przed whilem serwera
 
    #client_thread = ClientThread(client_socket)
    #client_thread.start()
-
-   while True:
-       receiver_id = input("Enter the id of the user you want to send a message to: ")
-       print("receiver id to",receiver_id)
-       send_id(client_socket, receiver_id)
-       print("send message")
-       time.sleep(0.1)  # Opóźnienie 0.1 sekundy
-       msg = receive_message(client_socket)
-       print(msg);
-       if msg.startswith("No such user:"):
-           print("if)")
-           print("msg: ", msg)
-           continue
-           #break
-       elif msg.startswith("You can send a message"):
-           print("else")
-           message = input("Enter your message: ")
-           send_message(client_socket, message)
-           break
-       else:
-           print("Received message: ", msg)
-
+   # Utwórz i uruchom wątki
+   send_thread = threading.Thread(target=send_messages, args=(client_socket,))
+   check_thread = threading.Thread(target=check_messages, args=(client_socket,))
+   send_thread.start()
+   check_thread.start()
 
 
 
