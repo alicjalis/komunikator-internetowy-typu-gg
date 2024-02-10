e7651c2347eff7c7ec41137d05e52b146cf5ff5b
alicjalis <57289626+alicjalis@users.noreply.github.com>
2024-02-10 21:13:41 +0100

    added latest client, receiving messages not working

--- main.py
+++ main.py
@@ -34,6 +34,15 @@ def receive_message(client_socket):
        msg_buffer += chunk
    return msg_buffer.decode('utf-8')
 
+def receive_all_messages(client_socket):
+    messages = []
+    while True:
+        msg = receive_message(client_socket)
+        if msg == "END_OF_MESSAGES\n":
+            break
+        messages.append(msg)
+    return messages
+
 
 def main():
    hostname = "192.168.80.130"
@@ -49,22 +58,30 @@ def main():
    print("Choose what you want to do: \n1 - send a message\n2 - check if you have any messages\n")
 
    while True:
-       receiver_id = input("Enter the id of the user you want to send a message to: ")
-       print("receiver id to",receiver_id)
-       send_id(client_socket, receiver_id)
-       time.sleep(0.1)  # Opóźnienie 0.1 sekundy
-       msg = receive_message(client_socket)
-       print(msg);
-       if msg.startswith("No such user:"):
-           print("if)")
-           print("msg: ", msg)
-           continue
-           #break
-       elif msg.startswith("You can send a message"):
-           print("else")
-           message = input("Enter your message: ")
-           send_message(client_socket, message)
-           continue
+       choice = input("Enter your choice: ")
+       client_socket.sendall(choice.encode('utf-8'))  # Wysłanie wyboru do serwera
+       if choice == "1":
+           receiver_id = input("Enter the id of the user you want to send a message to: ")
+           send_id(client_socket, receiver_id)
+           time.sleep(0.1)  # Opóźnienie 0.1 sekundy
+           msg = receive_message(client_socket)
+           if msg.startswith("No such user:"):
+               print(msg)
+               continue
+           elif msg.startswith("You can send a message"):
+               message = input("Enter your message: ")
+               send_message(client_socket, message)
+               continue
+       elif choice == "2":
+           messages = receive_all_messages(client_socket)
+           if messages:
+               print("Received messages:")
+               for msg in messages:
+                   print(msg)
+           else:
+               print("No messages")
+       else:
+           print("Invalid choice")
 
 
 
