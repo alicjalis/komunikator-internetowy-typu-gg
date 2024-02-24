ec0166f3418998a5875f2489ac85de319f8f2e4c
alicjalis <57289626+alicjalis@users.noreply.github.com>
2024-02-24 20:47:34 +0100

    added finished project

--- main.py
+++ main.py
@@ -1,119 +1,209 @@
 import socket
-import threading
-import time
+import os
+import tkinter
+from threading import Thread
+import signal
 
-# Funkcja łącząca się z serwerem
-def connect_to_server(hostname, port):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((hostname, port))
-    return client_socket
 
-# Funkcja wysyłająca nazwę użytkownika do serwera
-def send_username(client_socket, username):
-    client_socket.sendall(username.encode('utf-8'))
+def keyboardInterruptHandler(signal, frame):
+    if client_socket:
+        disconnect = "DISCONNECT"
+        client_socket.send(disconnect.encode())
+        client_socket.close()
+    if top:
+        top.destroy()
+    os._exit(1)
 
-# Funkcja wysyłająca identyfikator odbiorcy do serwera
-def send_id(client_socket, receiver_id):
-    client_socket.sendall(receiver_id.encode('utf-8'))
 
-# Funkcja odbierająca identyfikator od serwera
-def receive_id(client_socket):
-    id_buffer = bytearray()
+signal.signal(signal.SIGINT, keyboardInterruptHandler)
+
+def on_closing(event=None):
+    if client_socket:
+        disconnect = "DISCONNECT"
+        client_socket.send(disconnect.encode())
+        client_socket.close()
+    top.destroy()
+    os._exit(1)
+
+def raise_messages(frame):
+    frame.tkraise()
+    recipient = usr_name.get()
+
+    if recipient == "":
+        return
+
+    # usuwanie odczytanych juz wiadomosci
+    if recipient in unread_messages.keys():
+        del unread_messages[recipient]
+
+    labelvar.set(recipient)
+    msg_list.delete(0, tkinter.END)
+    mode = "r+"
+    if not(os.path.isfile("conversations/"+ my_username + "_" + recipient)):
+        mode = "a+"
+        # każdy klient ma osobny plik dla każdego nadawcy
+    with open("conversations/" + my_username + "_" + recipient, mode) as f:
+        lines = f.readlines()
+        for line in lines:
+            msg_list.insert(tkinter.END, line[:-1])  # usunięcie znaku nowej lini
+        f.close()
+
+def raise_menu(frame, usr_list):
+    frame.tkraise()
+    usr_name.set("")
+    refresh_menu()
+    usr_list.bind("<Double-Button-1>", user_clicked)  # Przypisanie zdarzenia kliknięcia na liście użytkowników
+
+def refresh_menu():
+    usr_list.delete(0, tkinter.END)
+    for user, unread_count in unread_messages.items():
+        usr_list.insert(tkinter.END, f"{user} ({unread_count})")
+
+def user_clicked(event):
+    selection = usr_list.curselection()
+    if selection:
+        index = selection[0]
+        username_with_count = usr_list.get(index)
+        username = username_with_count.split(" ")[0]  # Wyodrębnienie nazwy użytkownika
+        usr_name.set(username)  # Ustawienie nazwy użytkownika w polu tekstowym
+        raise_messages(messages_frame)  # Rozpoczęcie konwersacji z wybranym użytkownikiem
+
+def receive_messages():
+    # osobny watek ktory w petli caly czas odczytuje wiadomosci
     while True:
-        chunk = client_socket.recv(1)
-        if chunk == b'\n':
-            break
-        id_buffer += chunk
-    return id_buffer.decode('utf-8')
-
-# Funkcja wysyłająca wiadomość do serwera
-def send_message(client_socket, message):
-    client_socket.sendall(message.encode('utf-8'))
-
-# Funkcja odbierająca wiadomość od serwera
-def receive_message(client_socket):
-    msg_buffer = bytearray()
-    while True:
-        chunk = client_socket.recv(1)
-        if chunk == b'\n':
-            break
-        msg_buffer += chunk
-    return msg_buffer.decode('utf-8')
-
-def receive_messages(client_socket):
-    msg_buffer = bytearray()
-    while True:
-        chunk = client_socket.recv(1)
-        if not chunk:  # Jeśli chunk jest pusty, oznacza to koniec danych
-            break
-        msg_buffer += chunk
-    return msg_buffer.decode('utf-8')
-
-# Funkcja odbierająca wszystkie wiadomości od serwera
-def receive_all_messages(client_socket):
-    messages = []
-    while True:
-        msg = receive_message(client_socket)
-        print(msg)
-        if not msg:
-            break
-        messages.append(msg)
-        print(messages)
-    return messages
-
-def message_receiver(client_socket):
-    while getattr(threading.current_thread(), "running", True):
-        message = receive_message(client_socket)
-        if message:
-            print("Received message:", message)
-        else:
-            print("No messages received")
-        break  # Zakończ pętlę po odebraniu jednej wiadomości
-
-
-# Główna funkcja programu klienta
-def main():
-    hostname = "192.168.80.130"
-    port = 1234
-    username = input("Enter your username: ")
-
-    client_socket = connect_to_server(hostname, port)
-    send_username(client_socket, username)
-    print("Your id is: ", receive_id(client_socket))
-    usernames_and_ids_msg = client_socket.recv(4096).decode('utf-8')
-    print(usernames_and_ids_msg)
-
-    # client_thread = ClientThread(client_socket)
-    # client_thread.start()
-    print("Choose what you want to do: \n1 - send a message\n2 - check if you have any messages\n")
-
-    while True:
-        # TODO read sprawdza czy ma wiad, dostaje albo no messages albo jakies messages
-        choice = input("Enter your choice: ")
-        client_socket.sendall(choice.encode('utf-8'))  # Wysłanie wyboru do serwera
-        if choice == "1":
-            receiver_id = input("Enter the id of the user you want to send a message to: ")
-            send_id(client_socket, receiver_id)
-            time.sleep(0.1)  # Opóźnienie 0.1 sekundy
-            msg = receive_message(client_socket)
-            if msg.startswith("No such user:"):
-                print(msg)
+        try:
+            msg = client_socket.recv(1024).decode()
+            if len(msg) == 0:
                 continue
-            elif msg.startswith("You can send a message"):
-                message = input("Enter your message: ")
-                send_message(client_socket, message)
+
+            (sender, message) = str(msg).split(" ", 1)
+            message = sender + ": " + message
+
+            with open("conversations/" + my_username + "_" + sender, "a+") as f:
+                f.write(message + "\n")
+                f.close()
+
+            if sender != usr_name.get():
+                if sender not in unread_messages.keys():
+                    unread_messages[sender] = 0
+                unread_messages[sender] += 1
+                refresh_menu()
                 continue
-        elif choice == "2":
-            messages = client_socket.recv(4096).decode('utf-8')
-            print("Received messages:", messages)
-            messages = messages.split("\n")  # Dzielenie otrzymanej wiadomości na osobne linie
-            for msg in messages:
-                if msg:  # Sprawdź, czy wiadomość nie jest pusta
-                    sender, content = msg.split(":", 1)  # Podział na nadawcę i treść wiadomości
-                    print(f"{sender}: {content}")  # Wyświetlenie nadawcy i treści wiadomości
-        else:
-            print("Invalid choice")
+            msg_list.insert(tkinter.END, message)
+            msg_list.see(tkinter.END)
+
+        except OSError:
+            break
 
 
-if __name__ == "__main__":
-    main()
+def send(event=None):
+    msg = my_msg.get()
+    if msg == "":
+        return
+    my_msg.set("")
+    entry_field.config(width=10)
+    recipient = usr_name.get()
+    msgf = "SEND " + recipient + " " + my_username + " " + msg
+    client_socket.send(msgf.encode())
+
+    msg = "Ty: " + msg
+    msg_list.insert(tkinter.END, msg)
+
+    msg_list.see(tkinter.END)
+
+    with open("conversations/" + my_username + "_" + recipient, "a+") as f:
+        f.write(msg + "\n")
+        f.close()
+
+hostname = "192.168.80.130" # ip serwera
+port = 1234          #port serwera
+
+my_username = input("Username: ")
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+client_socket.connect((hostname, port))
+
+if not os.path.exists('conversations'):
+    os.mkdir('conversations')
+
+username = "LOGIN " + my_username
+client_socket.send(username.encode())
+
+def rgb_hack(rgb):
+    return "#%02x%02x%02x" % rgb
+
+top = tkinter.Tk()
+top.iconbitmap('icon.ico')
+top.title("Chat - " + my_username)
+
+usr_name = ""
+unread_messages = {}
+
+messages_frame = tkinter.Frame(top)
+
+my_msg = tkinter.StringVar()
+
+scrollbar = tkinter.Scrollbar(messages_frame)
+msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
+scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=True)
+msg_list.pack()
+
+messages_frame.grid(row=0, column=0, sticky="nsew")  # sticky="nsew" pozwoli na rozciąganie się ramki
+messages_frame.config(bg=rgb_hack((220, 220, 220)))
+labelvar = tkinter.StringVar()
+label = tkinter.Label(messages_frame, textvariable=labelvar)
+label.config(bg=rgb_hack((220, 220, 220)))
+label.pack()
+
+def update_entry_width(event=None):
+    entry_width = len(my_msg.get()) + 5  # Adjust this value as needed for padding
+    entry_field.config(width=entry_width)
+
+entry_field = tkinter.Entry(messages_frame, textvariable=my_msg)
+entry_field.bind("<Return>", send)
+entry_field.bind("<KeyRelease>", update_entry_width)  # Bind the update function to key release event
+entry_field.pack(fill=tkinter.X)
+
+send_button = tkinter.Button(messages_frame, text="Wyslij", bg='blue', fg='white', command=send)
+send_button.pack()
+back_button = tkinter.Button(messages_frame, text="Wyjdz z rozmowy",  bg='blue', fg='white', command=lambda: raise_menu(menu_frame, usr_list))
+back_button.pack()
+
+menu_frame = tkinter.Frame(top)
+usr_name = tkinter.StringVar()
+usr_name.set("Type name of user")
+
+menu_frame.grid(row=0, column=0, sticky="nsew")  # sticky="nsew" pozwoli na rozciąganie się ramki
+menu_frame.config(bg=rgb_hack((220, 220, 220)))
+
+scrollbar2 = tkinter.Scrollbar(menu_frame)
+usr_list = tkinter.Listbox(menu_frame, height=15, width=50, yscrollcommand=scrollbar2.set)
+scrollbar2.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+usr_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=True)
+usr_list.pack()
+
+label_menu = tkinter.Label(menu_frame, text="Nazwa uzytkownika:")
+label_menu.config(bg=rgb_hack((220, 220, 220)))
+label_menu.pack()
+
+name_field = tkinter.Entry(menu_frame, textvariable=usr_name)
+name_field.pack(fill=tkinter.X)  # fill=tkinter.X pozwoli na rozciąganie się pola tekstowego wzdłuż osi X
+
+name_button = tkinter.Button(menu_frame, text="Potwierdz", bg='blue', fg='white',command=lambda: raise_messages(messages_frame))
+name_button.pack()
+
+top.protocol("WM_DELETE_WINDOW", on_closing)
+
+receive_thread = Thread(target=receive_messages)
+receive_thread.start()
+
+raise_menu(menu_frame, usr_list)
+
+# Konfiguracja rozciągania się ramki w oknie głównym
+top.grid_rowconfigure(0, weight=1)
+top.grid_columnconfigure(0, weight=1)
+
+tkinter.mainloop()
